f4765c3a324afe2cf0709f6600ad810e156069d5
PauTG <paulit.codes@gmail.com>
2023-04-07 18:24:03 -0300

    apply selector

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn_modoDark = document.getElementById('modoDark');
+const htmlModoDark = document.getElementById('Dark');
+
+
+btn_modoDark.addEventListener('click', function(){
+        document.getElementById('Dark').classList.toggle('dark');
+});
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: ["index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
